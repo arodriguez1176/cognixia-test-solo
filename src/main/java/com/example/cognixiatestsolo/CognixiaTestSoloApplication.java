69f8d8018fb19d28e3d3e8aed5aa7d26fb04f1a9
Armando Rodriguez <avr5454@gmail.com>
2022-03-10 17:27:00 -0500

    cognixia-test-solo initialization

---
+++ src/main/java/com/example/cognixiatestsolo/CognixiaTestSoloApplication.java
@@ -0,0 +1,13 @@
+package com.example.cognixiatestsolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognixiaTestSoloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CognixiaTestSoloApplication.class, args);
+	}
+
+}
